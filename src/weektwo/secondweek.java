450762b00508a83e186682fde5e4acdd3a12f970
Melanie lund <melaniepund@gmail.com>
2018-10-10 16:49:04 -0700

    try again

---
+++ src/weektwo/secondweek.java
@@ -0,0 +1,29 @@
+
+package weektwo;
+
+public class secondweek {
+
+	public static void main(String[] args) {
+		
+	
+	//booleans are typically used to see if further action is needed. 
+	// in order to evaluate boolean data in our code we can use boolean operators 
+
+
+	boolean expression = 1 == 1; 
+	
+	boolean greaterthan = 4 > 2; 
+	
+	boolean twothingstrue = 1 == 2 && 2 == 2; 
+	
+	boolean onethingtrue = 1 == 2 || 2 == 2; 
+	
+
+	System.out.println(onethingtrue);
+	
+
+	
+	}
+
+
+}
