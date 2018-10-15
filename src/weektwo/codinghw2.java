bb5a575822623e18d3662f769b95fdf69d21d1bc
Melanie lund <melaniepund@gmail.com>
2018-10-14 21:31:52 -0700

    homework final

--- src/weektwo/codinghw2.java
+++ src/weektwo/codinghw2.java
@@ -7,11 +7,11 @@ public class codinghw2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	
-	Scanner reader = new Scanner(System.in);
+	Scanner sc = new Scanner(System.in);
 	
 	System.out.println("Please enter your name:"); 
 
-	String name = reader.nextLine();
+	String name = sc.nextLine();
 	
 	int input = 0;
 	
@@ -20,51 +20,49 @@ public class codinghw2 {
 	
 	
 	
+	String answer = sc.nextLine();
 	
-	
-	String answer = "yes";
-	 
-	
-	 {
-	System.out.println("(1) Door number 1");
-	System.out.println("(2) Door Number 2");
-	System.out.println("(3) Door number 3");
-	System.out.println("(4) Door number 4");
-	
+	if ( answer.equals("yes")) {
+		System.out.println("(1) Door number 1");
+		System.out.println("(2) Door Number 2");
+		System.out.println("(3) Door number 3");
+		System.out.println("(4) Door number 4");
+		
+		input = sc.nextInt();
+		
+		
+		
+		
+		String res = "Congratulations you have won ";
+		
+		switch(input)   { 
+			case 1: 
+				System.out.println( res + " a couple milky ways");
+				break;
+				
+			case 2: 
+				System.out.println( res + " a weekend with Elliott");
+				break;
+				
+			case 3: 
+				System.out.println( res + " a new desk chair");
+				break;
+				
+			case 4:
+				System.out.println(res + "a new iphone ");
+			
+			case 5: 
+				System.out.println( " Thank you for playing");
+				break; 
+		}	
+	} else { 
+		
+		System.out.println("Goodbye!");
+		
 	}
 	
 	
-	String res = "Congratulations you have won ";
 	
-	switch(input)   { 
-		case 1: 
-			System.out.println( res + " a couple milky ways");
-			break;
-			
-		case 2: 
-			System.out.println( res + " a weekend with Elliott");
-			break;
-			
-		case 3: 
-			System.out.println( res + " a new desk chair");
-			break;
-			
-		case 4:
-			System.out.println(res + "a new iphone ");
-		
-		case 5: 
-			System.out.println( " Thank you for playing");
-			break; 
-	
-			
-		
-		
-				
-				
-				
-			
-
-}
 	
 	}
 	
